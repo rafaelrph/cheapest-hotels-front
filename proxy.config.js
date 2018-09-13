d43e5976ec85ee0cb1f01c3b4326606c5a394516
Rafael Barros <rafael.rph7@gmail.com>
2018-09-12 22:22:16 -0700

    Test Heroku

--- proxy.config.js
+++
@@ -1,9 +0,0 @@
-const proxy = [
-    {
-        context: '/api',
-        target: 'https://aw-cheapest-hotels-api.herokuapp.com/',
-        pathRewrite: {'^/api' : ''}
-    }
-];
-
-module.exports = proxy;
--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + "/dist/index.html"));
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 console.log("Listening");
